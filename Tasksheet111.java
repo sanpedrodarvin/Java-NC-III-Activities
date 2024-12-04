5648f033289f1dcca3d16b4529e80ff4ff3ff567
sanpedrodarvin <sanpedrodarvin@gmail.com>
2024-12-04 10:16:19 +0800

    Tasksheet111

---
+++ Tasksheet111.java
@@ -0,0 +1,10 @@
+public class Tasksheet111 {
+    /**
+     * The HelloWorldApp class implements an application that
+     * simply prints "Hello World!" to standard output.
+     */
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+
